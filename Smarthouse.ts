e4c238a1712e3fa6bd52218e5077b2201e8de665
samlcy1997 <samlcy1997@gmail.com>
2020-08-12 14:59:45 +0800

    update block weight

--- Smarthouse.ts
+++ Smarthouse.ts
@@ -1,7 +1,7 @@
 /**
  * Custom blocks
  */
-//% color=#ffba52 icon="\uf015" block="Smarthouse"
+//% weight=98 color=#ffba52 icon="\uf015" block="Smarthouse"
 namespace House {
     let light_variable = 0
     let temperature_variable = 0
