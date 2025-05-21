b4bec06845ac2bab62fcd780fd0a4b99ccc0286b
Larry-lai-smarthon <larry.lai.smarthon@gmail.com>
2025-05-21 10:33:38 +0800

    update the test function

--- test.ts
+++ test.ts
@@ -2,23 +2,41 @@
 
 /* 
 Test procedure:
-1. get light value (percentage) at Pin
-2. get flame detection at pin
-3. get temperature at pin
-4. get humidity at pin
+1. turn on the motor
+2. turn on the 180 servo
+3. turn on the 360 servo
+4. turn off the motor
+5. get light value (percentage) at Pin
+6. get temperature at pin
+7. get humidity at pin
+8. get IAQ at pin
+9. get distance at pin
 */
 
 //get the light value, flame detection, temperature and humidity
+input.onButtonPressed(Button.A, function () {
+    House.TurnMotor(512, AnalogPin.P0)
+})
+input.onButtonPressed(Button.AB, function () {
+    House.Turn360Servo(House.ServoDirection.Clockwise, House.ServoSpeed.Level3, AnalogPin.P2)
+})
+input.onButtonPressed(Button.B, function () {
+    House.Turn180Servo(90, AnalogPin.P1)
+})
+House.Button(House.PressButtonList.B2, function () {
+    House.TurnMotor(0, AnalogPin.P0)
+    House.Turn180Servo(0, AnalogPin.P1)
+    House.Turn360Servo(House.ServoDirection.Clockwise, House.ServoSpeed.Level0, AnalogPin.P2)
+})
+OLED.init(128, 64)
 basic.forever(function () {
-    House.readDht11(DigitalPin.P0)
-    basic.showString("Light:" + House.getLight(AnalogPin.P1))
-    if (House.getFlame(DigitalPin.P2)) {
-        basic.showIcon(IconNames.Happy)
-    } else {
-        basic.showIcon(IconNames.Sad)
-    }
-    basic.showString("Distance:" + House.read_distance_sensor_home(House.DistanceUnit.Centimeters, DigitalPin.P14, DigitalPin.P15))
-    basic.showString("Temperature:" + House.readTemperatureData(House.TempDegree.DegreeCelsius))
+    OLED.clear()
+    House.readDht11(DigitalPin.P3)
+    basic.showString("Light:" + House.getLight(AnalogPin.P4))
+    basic.showString("dis:" + House.read_distance_sensor_home(House.DistanceUnit.Centimeters, DigitalPin.P14, DigitalPin.P15))
+    basic.showString("temp:" + House.readTemperatureData(House.TempDegree.DegreeCelsius))
     basic.showString("Humidity:" + House.readHumidityData())
+    basic.showString("IAQ" + House.getIAQ())
     basic.pause(1000)
 })
+
